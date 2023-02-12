6cb0de74773b032fa4aaea0a4c918198c4bb5b1a
Zirui Yin <zy132@duke.edu>
2023-02-12 16:06:20 -0500

    db initilised

---
+++ db/gen.py
@@ -0,0 +1,43 @@
+import csv
+import random
+from faker import Faker
+fake = Faker()
+
+num_user = 10000
+num_movie = 20
+min_age = 10
+max_age = 80
+
+userlist = []
+ratinglist = []
+
+def gen(num_user):
+    for i in range(num_user):
+        name = fake.unique.name()
+        age = random.randint(min_age, max_age)
+        row = [i, name, age]
+        userlist.append(row)
+        movieset = set()
+        while len(movieset) < 10:
+            movieset.add(random.randint(0, num_movie))
+        for mid in movieset:
+            age_movie = random.randint(min_age, age)
+            rowr = [i, mid, random.randint(0, 10), age_movie]
+            ratinglist.append(rowr)
+
+def write_user():
+    with open('user.csv', 'w') as f:
+        writer = csv.writer(f)
+        for row in userlist:
+            writer.writerow(row)
+
+def write_rating():
+    with open('rating.csv', 'w') as f:
+        writer = csv.writer(f)
+        for row in ratinglist:
+            writer.writerow(row)
+
+
+gen(num_user)
+write_user()
+write_rating()
